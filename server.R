3b2ea5b4e10e697fd563b47aef3d706099bcb3e5
David Hart <138690175+djhart-wgnhs@users.noreply.github.com>
2025-08-18 14:50:20 -0500

    Update server.R

--- server.R
+++ server.R
@@ -218,8 +218,8 @@ function(input, output, session) {
     no3upr <- format(round(nO3Prediction()$upr, digits = 1), nsmall = 1)
     no3Units <- "mg/L"
     paste0(h2("Chart Explanation"),
-           "This bar chart shows the break down of land cover for the groundwater entry points", "<br>"
-           # Removing line with N estimation "Based on a correlation with land cover, the the nitrate level for the region you selected is likely between ", no3lwr, " ", no3Units, " and ", no3upr, " ", no3Units)
+           "This bar chart shows the break down of land cover for the groundwater entry points", "<br>",
+           "Based on a correlation with land cover, the the nitrate level for the region you selected is likely between ", no3lwr, " ", no3Units, " and ", no3upr, " ", no3Units)
   })
   
   output$transitTimeExplainer <- renderText({
